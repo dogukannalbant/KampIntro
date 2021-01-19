20509a50bea2d78818aea2a0d5b6784e64363745
dogukan nalbant <yirmikurus@gmail.com>
2021-01-19 15:26:54 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+
+            //array - dizi
+            //tek tek tanımlamak yerine arraylerde tutuyoruz
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya başlangıç için temel kurs", "Java", "Python" };
+
+
+
+            for (int i = 0; i < kurslar.Length; i++) //kurslar.Length  Lrnght = eleman sayısını verir. 0 dan başlar kursların eleman sayıları kadar döner.
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti"); // ayrımı göstermek için yazdık.
+
+            foreach (string kurs in kurslar) // buradaki kurs her bir elemanı dolaşırken verdiğimiz takma isimdir - alliastır.
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        // naming convention
+        public void Ekle(Urun urun) // büyük Urun tipi küçük urun parametresi
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi: " + urun.Adi);
+
+        }
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi: " + urunAdi);
+        }
+
+    }
+}
