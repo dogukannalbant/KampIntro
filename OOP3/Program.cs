a3bc3d0f6375e9ffd7f7d1350f6608473fe36463
dogukan nalbant <yirmikurus@gmail.com>
2021-01-24 00:09:49 +0300

    23 ocak ders sonu

---
+++ OOP3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();                     
+            IKrediManager taksitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DataBaseLoggerService();
+            ILoggerService fileLogerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager, databaseLoggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, taksitKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
---
+++ Oop2/TuzelMusteri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    //miras == inheritance (Tüzel müşteri bir müşteridir)
+    class TuzelMusteri: Musteri
+    {
+        
+        public string SirketAdi { get; set; }
+        public string VergiNo { get; set; }
+    }
+}
